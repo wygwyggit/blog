2251c8403dcd10761859d3a91aa4d1a6af0c6bf0
yungen_wang <yungen_wang@woku.net>
2024-03-15 22:22:42 +0800

    first commit

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "我酷前端小站",
+  description: "分享前端与科技生活",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '工作', link: '/work', activeMatch: '/work/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: {
+      "/work/":
+      {
+        base: '/work/',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
